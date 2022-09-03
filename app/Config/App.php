1b84d9253bde950d97405fd7178e31cd3739a508
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-09-03 23:27:00 +0545

    Removing public and index.php from url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'http://moneymanagerex.ipx.co.uk/';
+    public $baseURL = 'http://localhost/codeigtuto/';
 
     /**
      * --------------------------------------------------------------------------
@@ -55,7 +55,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $uriProtocol = 'REQUEST_URI';
+    public $uriProtocol = 'PATH_INFO';
 
     /**
      * --------------------------------------------------------------------------
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 // Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
@@ -17,7 +17,7 @@ chdir(__DIR__);
 
 // Load our paths config file
 // This is the line that might need to be changed, depending on your folder structure.
-$pathsConfig = FCPATH . '../app/Config/Paths.php';
+$pathsConfig = FCPATH . 'app/Config/Paths.php';
 
 // ^^^ Change this if you move your application folder
 require realpath($pathsConfig) ?: $pathsConfig;
