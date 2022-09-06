76321f11e3abf8aadd5551d2767212542ca9c616
Kailash Kumar <91149321+KailashKumarx7@users.noreply.github.com>
2022-09-06 23:12:10 +0545

    passing data to views, Data iteration in views using Codeigniter 4

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -1,21 +1,42 @@
 <?php
-namespace App\Controllers;
+
+
+
+// namespace App\Controllers;
 
 
 // if want to use name space we use this
 // use CodeIgniter\Controller;
 
 
-//other wise simplay add CodeIgniter here
-class Blog extends \CodeIgniter\Controller
-{
-    public function index(){
-        echo view('header');
-        echo view('myview');
-        echo view('footer');
+// //other wise simplay add CodeIgniter here
+// class Blog extends \CodeIgniter\Controller
+// {
+//     public function index(){
+//         echo view('header');
+//         echo view('myview');
+//         echo view('footer');
+//     }
+// }
+
+
+
+
+
+namespace App\Controllers;
+
+class Blog extends \CodeIgniter\Controller{
+
+    public function index()
+    {
+        $data = [
+            "page_title"=>"CodeIgniter 4",
+            "page_heading"=>"CodeIgniter 4 views",
+            "subjects"=>["HTML","CSS","Bootstrap","Javascript","AJAX","JSON","JQuery","PHP","MYSQL","CodeIgniter","Laravel"],
+        ];
+        echo view("myview",$data);
     }
 }
-
 
 
 
--- app/Views/myview.php
+++ app/Views/myview.php
@@ -1,9 +1,27 @@
 <html>
 <head>
-    
-    <title>Welcome to codeigtuto</title>
+    <!--templating engine-->
+    <title><?= $page_title;?></title>
 </head>
 <body>
-    <h1>Hello Welcome to my Tuto proj and main section </h1>
+    <h1><?=$page_heading;?> </h1>
+    <h3>subject List</h3>
+    <ul>
+     <?php
+     if(count($subjects)>0):
+     foreach($subjects as $sub):
+     ?>
+         <li><?=$sub;?></li>
+        
+
+    <?php 
+     endforeach;
+    else:
+        echo "<p>Sorry! NO record found";
+    
+    endif;
+     
+     ?>
+    </ul>
 </body>
 </html>
